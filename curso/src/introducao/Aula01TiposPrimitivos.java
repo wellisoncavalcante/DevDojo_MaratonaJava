3753c9814237a69312db2c492521368651b4373d
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-17 18:55:02 -0300

    Commit inicial

---
+++ curso/src/introducao/Aula01TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package introducao;
+
+public class Aula01TiposPrimitivos{
+    public static void main(String[] args){
+        // int, double, float, char, byte, short, long e boolean
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 20000;
+        float salarioFloat = 2500;
+        byte idadeByte = -128;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+        String nome = "Wellison Cavalcante";
+        System.out.println("A idade é "+idade+" anos");
+        System.out.println(verdadeiro);
+        System.out.println("char "+caractere);
+        System.out.println(numeroGrande);
+        System.out.println(nome);
+    };
+}
