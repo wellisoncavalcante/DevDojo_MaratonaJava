fa16adce88ed319023a9a263b87b666526ee492b
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-17 19:20:17 -0300

    commit aula 06

---
+++ curso/src/introducao/Aula06EstruturasDeRepeticaoExercicio02.java
@@ -0,0 +1,15 @@
+package introducao;
+
+public class Aula06EstruturasDeRepeticaoExercicio02 {
+    static void main() {
+        for (int i = 0 ; i <= 50; i++){
+            if (i > 25){
+                break;
+            }
+            System.out.println(i);
+        }
+
+
+
+    }
+}
