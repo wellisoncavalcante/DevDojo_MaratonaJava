a6dce356b41944e9f4f3d1fec9a4ae54f211f46e
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-17 19:00:25 -0300

    commit aula 02

---
+++ curso/src/introducao/Aula02TiposPrimitivosExercicio.java
@@ -0,0 +1,13 @@
+package introducao;
+
+public class Aula02TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Wellison Cavalcante";
+        String endereco = "Rua Fernando 95 A";
+        double salario = 20000;
+        String dataRecebimentoSalario = "30/10/2032";
+        String relatorio = ("Eu "+nome+ " morando no endereço "+endereco+ " confirmo que recebi o salário "+salario+", na data "+dataRecebimentoSalario);
+
+        System.out.println(relatorio);
+    }
+}
