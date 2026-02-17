a93a49220f2b89d521a0544d26ed97dbc0bc842b
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-17 19:17:42 -0300

    commit aula 04

---
+++ curso/src/introducao/Aula04EstruturasCondicionais01.java
@@ -0,0 +1,24 @@
+package introducao;
+
+public class Aula04EstruturasCondicionais01 {
+    public static void main(String[] args) {
+
+        int idade = 18;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebida alcólica.");
+        } else{
+                System.out.println("Não autorizado a comprar bebida alcólica.");
+            }
+
+        if (!isAutorizadoComprarBebida) {
+            System.out.println("Não autorizado a comprar bebida alcólica.");
+        }
+        boolean c = false;
+        if(c ==true);
+        System.out.println("Dentro de algo que nunca deve ser feito");
+
+        System.out.println("Fora do if");
+    }
+}
