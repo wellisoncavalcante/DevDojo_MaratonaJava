d0183d0dde100bb8bb96288fb85ae3210cf81b86
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-17 19:20:58 -0300

    commit aula 06

---
+++ curso/src/introducao/Aula06EstruturasDeRepeticaoExercicio04.java
@@ -0,0 +1,14 @@
+package introducao;
+
+public class Aula06EstruturasDeRepeticaoExercicio04 {
+    public static void main(String[] args) {
+        double valorTotal = 30000;
+        for (int parcela = (int) valorTotal ; parcela >= 1 ; parcela--) {
+            double valorParcela = valorTotal / parcela;
+            if(valorParcela < 1000) {
+                continue;
+            }
+            System.out.println("Parcela "+parcela+ " R$ "+valorParcela);
+        }
+    }
+}
