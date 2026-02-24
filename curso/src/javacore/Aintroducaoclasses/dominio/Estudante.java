187e4701ade2263200ac8ee2c2ae6c60c0134bed
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-24 18:32:52 -0300

    estudante

---
+++ curso/src/javacore/Aintroducaoclasses/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package javacore.Aintroducaoclasses.dominio;
+
+public class Estudante {
+    public String nome = "Luffy, o pirata do chapéu de palha!";
+    public int idade;
+    public char sexo;
+}
