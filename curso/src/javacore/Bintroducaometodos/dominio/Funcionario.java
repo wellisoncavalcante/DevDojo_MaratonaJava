2ec01deb183b4fe40974250513aa2cbb87641fda
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-24 18:36:24 -0300

    -

---
+++ curso/src/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -0,0 +1,53 @@
+package javacore.Bintroducaometodos.dominio;
+
+public class Funcionario {
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media = 0;
+
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        for (double salario : salarios) {
+            System.out.print(salario + " ; ");
+        }
+        imprimeMediaSalario();
+    }
+    public void imprimeMediaSalario() {
+        double media = 0;
+        for (double salario : salarios) {
+            media += salario;
+        }
+        media /= salarios.length;
+        System.out.println("\nMédia salarial: " + media);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+}
