9041fc18dbc4c7633c859fa022838ebf0c9fdb12
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-24 18:36:14 -0300

    -

---
+++ curso/src/javacore/Bintroducaometodos/dominio/CalculadoraDesafio02.java
@@ -0,0 +1,12 @@
+package javacore.Bintroducaometodos.dominio;
+
+public class CalculadoraDesafio02 {
+    public void dobro(){
+    int [] listaNum = new int[5];
+        System.out.println(listaNum[0]);
+        System.out.println(listaNum[1]);
+        System.out.println(listaNum[2]);
+        System.out.println(listaNum[3]);
+        System.out.println(listaNum[4]);
+    }
+}
