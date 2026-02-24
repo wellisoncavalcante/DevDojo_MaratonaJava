a508166777ad5a7717aa7a0f6e7f25162b4d8a73
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-24 18:35:07 -0300

    -

---
+++ curso/src/javacore/Bintroducaometodos/teste/CalculadoraTeste01.java
@@ -0,0 +1,11 @@
+package javacore.Bintroducaometodos.teste;
+
+import javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTeste01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
+    }
+}
