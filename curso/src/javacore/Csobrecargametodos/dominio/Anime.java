3969fa84b6e6bad6178cdea68a70c5a94bc5e889
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-25 17:06:30 -0300

    -

---
+++ curso/src/javacore/Csobrecargametodos/dominio/Anime.java
@@ -0,0 +1,59 @@
+package javacore.Csobrecargametodos.dominio;
+
+public class Anime {
+    private String tipo;
+    private int episodios;
+    private String nome;
+    private String genero;
+
+    public void init(String nome, String tipo, int episodios) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+    }
+
+    public void init(String nome, String tipo, int episodios, String genero) {
+        this.init(nome, tipo, episodios);
+        this.genero = genero;
+    }
+
+    public void imprime() {
+        System.out.println(this.tipo);
+        System.out.println(this.episodios);
+        System.out.println(this.nome);
+        System.out.println(this.genero);
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setEpisodios(int episodios) {
+        this.episodios = episodios;
+    }
+
+    public int getEpisodios() {
+        return episodios;
+    }
+
+}
