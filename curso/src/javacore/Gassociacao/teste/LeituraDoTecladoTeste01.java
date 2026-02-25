edf2c246efc13f3cd04454da2a2241f01c812b9e
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-25 17:10:01 -0300

    -

---
+++ curso/src/javacore/Gassociacao/teste/LeituraDoTecladoTeste01.java
@@ -0,0 +1,23 @@
+package javacore.Gassociacao.teste;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTeste01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = input.nextLine();
+
+        System.out.println("Digite a sua idade: ");
+        int idade = input.nextInt();
+
+        System.out.println("Digite M ou F para seu sexo");
+        char sexo = input.next().charAt(0);
+
+        System.out.println("Nome: "+nome);
+        System.out.println("Idade: "+idade);
+        System.out.println("Sexo: "+sexo);
+
+    }
+}
