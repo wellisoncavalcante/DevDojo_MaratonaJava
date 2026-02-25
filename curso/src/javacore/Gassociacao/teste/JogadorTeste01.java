51d422e86f8883fdcf52590ad6826110cae0c3d7
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-25 17:09:54 -0300

    -

---
+++ curso/src/javacore/Gassociacao/teste/JogadorTeste01.java
@@ -0,0 +1,15 @@
+package javacore.Gassociacao.teste;
+
+import javacore.Gassociacao.dominio.Jogador;
+
+public class JogadorTeste01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Jogador jogador2 = new Jogador("Romário");
+        Jogador jogador3 = new Jogador("Cafu");
+        Jogador[] jogadores = new Jogador[]{jogador1, jogador2, jogador3};
+        for (Jogador jogador : jogadores){
+            jogador.imprime();
+        }
+    }
+}
