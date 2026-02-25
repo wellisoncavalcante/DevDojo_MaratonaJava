0eef59c3e1ee7cbea95bad2e8406e2fbfca4eef7
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-25 17:09:06 -0300

    -

---
+++ curso/src/javacore/Fmodificadorestatico/teste/AnimeTeste01.java
@@ -0,0 +1,11 @@
+package javacore.Fmodificadorestatico.teste;
+
+import javacore.Fmodificadorestatico.dominio.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+    }
+}
