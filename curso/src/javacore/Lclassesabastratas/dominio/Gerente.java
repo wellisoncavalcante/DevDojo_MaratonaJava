de24651913c19d9a69a502637432bd78a14c0c26
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-26 17:34:57 -0300

    -

---
+++ curso/src/javacore/Lclassesabastratas/dominio/Gerente.java
@@ -0,0 +1,20 @@
+package javacore.Lclassesabastratas.dominio;
+
+public class Gerente extends Funcionario {
+    public Gerente(String nome, double salario) {
+        super(nome, salario);
+    }
+
+    @Override
+    public void calculaBonus() {
+        this.salario = this.salario + this.salario * 0.20;
+    }
+
+    @Override
+    public String toString() {
+        return "Gerente{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+}
