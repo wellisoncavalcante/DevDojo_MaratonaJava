d369735f75d0ea16059a8b13307a63d2c51b4453
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-26 17:34:20 -0300

    -

---
+++ curso/src/javacore/Kenum/teste/ClienteTeste01.java
@@ -0,0 +1,22 @@
+package javacore.Kenum.teste;
+
+import javacore.Kenum.dominio.Cliente;
+import javacore.Kenum.dominio.TipoCliente;
+import javacore.Kenum.dominio.TipoPagamento;
+
+public class ClienteTeste01 {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Leon Kennedy", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Luis Serra", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+
+        System.out.println(cliente1);
+        System.out.println(cliente2);
+
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente.getNomeRelatorio());
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Física");
+        System.out.println(tipoCliente2);
+    }
+}
