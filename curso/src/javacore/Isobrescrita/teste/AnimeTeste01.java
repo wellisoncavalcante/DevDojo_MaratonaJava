dbb27061c7f8d2525719a4294e9b2b8133be2220
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-26 17:34:12 -0300

    -

---
+++ curso/src/javacore/Isobrescrita/teste/AnimeTeste01.java
@@ -0,0 +1,11 @@
+package javacore.Isobrescrita.teste;
+
+import javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Fullmetal Alchemist");
+
+        System.out.println(anime);
+    }
+}
