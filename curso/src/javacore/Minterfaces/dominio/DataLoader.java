4824eabe5ed72c1114e38594a2f560465a0b2872
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-27 18:31:16 -0300

    -

---
+++ curso/src/javacore/Minterfaces/dominio/DataLoader.java
@@ -0,0 +1,15 @@
+package javacore.Minterfaces.dominio;
+
+public interface DataLoader {
+    public static final int MAX_DATA_SIZE = 10;
+    public abstract void load();
+
+    default void checkPermission(){
+        System.out.println("Fazendo checagem de permissões");
+    }
+
+    public static void retrieveMaxDataSize(){
+        System.out.println("Dentro do retrieveMaxDataSize na interface");
+    }
+
+}
