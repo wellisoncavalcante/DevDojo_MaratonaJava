c0288ad5bfba090c29b7f8625da963b52eff4a39
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-27 18:31:14 -0300

    -

---
+++ curso/src/javacore/Minterfaces/dominio/DatabaseLoader.java
@@ -0,0 +1,24 @@
+package javacore.Minterfaces.dominio;
+
+public class DatabaseLoader implements DataLoader, DataRemover {
+    // private -> default -> protected -> public
+
+    @Override
+    public void load() {
+        System.out.println("Carregando dados do banco de dados");
+
+    }
+
+    @Override
+    public void remove() {
+        System.out.println("Removendo dados do banco de dados");
+    }
+
+    @Override
+    public void checkPermission() {
+        System.out.println("Checando permissões no banco de dados");
+    }
+    public static void retrieveMaxDataSize(){
+        System.out.println("Dentro do retrieveMaxDataSize na classe DatabaseLoader");
+    }
+}
