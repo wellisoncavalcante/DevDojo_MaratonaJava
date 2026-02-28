bcdcfa41b1c8c4dc45fe43f9c48b2957b5c9568f
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-28 20:45:34 -0300

    -

---
+++ curso/src/javacore/Oexecption/dominio/Pessoa.java
@@ -0,0 +1,9 @@
+package javacore.Oexecption.dominio;
+
+import java.io.IOException;
+
+public class Pessoa {
+    public void salvar() throws LoginInvalidoException, IOException {
+        System.out.println("Salvando pessoa");
+    }
+}
