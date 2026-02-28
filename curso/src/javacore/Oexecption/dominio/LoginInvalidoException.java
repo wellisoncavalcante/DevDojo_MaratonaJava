2bfd0d4141aea86b7f8700929f4596eee6cfad6f
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-02-28 20:45:29 -0300

    -

---
+++ curso/src/javacore/Oexecption/dominio/LoginInvalidoException.java
@@ -0,0 +1,12 @@
+package javacore.Oexecption.dominio;
+
+public class LoginInvalidoException extends Exception {
+
+    public LoginInvalidoException() {
+        super("Login inválido");
+    }
+
+    public LoginInvalidoException(String message) {
+        super(message);
+    }
+}
